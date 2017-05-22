677ec4c88cfca398968df004c790bef29743a639
Lexine Langford <tanuki360@hotmail.com>
2017-05-22 22:17:05 +0700

    Update all pin to current relay available (GPIOsetup)

--- test.py
+++ test.py
@@ -9,8 +9,8 @@ _AnkleOpen = 27
 _AnkleClose = 22
 _BaseCW = 23
 _BaseCCW = 24
-_AnkelCW =10
-_AnkelCCW =9
+_AnkelCW = 10
+_AnkelCCW = 9
 
 On = 0
 Off = 1
@@ -19,15 +19,27 @@ Off = 1
 def GPIOsetup():
         GPIO.setwarnings(False)
         GPIO.setmode(GPIO.BCM)
+        #set to output mode
         GPIO.setup(_GrapOpen, GPIO.OUT)
         GPIO.setup(_GrapClose, GPIO.OUT)
         GPIO.setup(_AnkleOpen, GPIO.OUT)
         GPIO.setup(_AnkleClose, GPIO.OUT)
+        GPIO.setup(_BaseCW, GPIO.OUT)
+        GPIO.setup(_BaseCCW, GPIO.OUT)
+        GPIO.setup(_AnkelCW, GPIO.OUT)
+        GPIO.setup(_AnkelCCW, GPIO.OUT)
+       
         
+        
+        # Turn off all
         GPIO.output(_GrapOpen, Off)
         GPIO.output(_GrapClose, Off)
         GPIO.output(_AnkleOpen, Off)
         GPIO.output(_AnkleClose, Off)
+        GPIO.output(_BaseCW, Off)
+        GPIO.output(_BaseCCW, Off)
+        GPIO.output(_AnkelCW, Off)
+        GPIO.output(_AnkelCCW, Off)
 
 def GrapOpen():
         GPIOsetup()
